b100f1834902050fa705b2ad6970fd5284474392
Kevin Wandji <kevin.wandjikonkep001@stud.fh-dortmund.de>
2025-04-12 00:13:35 +0200

    My first changes of main method with square

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,11 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         System.out.println("Hello, Test!");
+
+        System.out.println("Le carré de 2 est le chiffre " + square(2));
+    }
+
+    public static int square(int a) {
+        return a * a;
     }
 }
